9293e2e62b8e11f12249c98f510c09082ed1b9de
pasqualecicinelli <pasquale.cicinelli89@gmail.com>
2023-08-28 20:55:44 +0200

    fix for - function and button

--- JS/main.js
+++ JS/main.js
@@ -26,39 +26,102 @@ const images = [
     },
 ];
 
-let cardPicture = document.getElementById('card-picture');
-let cardImg = document.getElementsByClassName('card-img');
-let cardTitle = document.getElementById('title');
-let cardDescription = document.getElementById('description');
+
+let cardIdElem = document.getElementById('container-card');
+let cardElem = document.getElementsByClassName('card');
+
 let buttonBack = document.getElementById('btn-back');
 let buttonNext = document.getElementById('btn-next');
 
-let activeImg = 0;
-
-buttonNext.addEventListener("click", function () {
-
-//non funziona
-
-    const activePicture = document.querySelector(".cardImg");
-
-    cardPicture.classList.remove('active');
-
-    if (activeImg <= images.image) {
-
-        activeImg++;
-        let newCardPicture = cardPicture;
-        newCardPicture.classList.add('active');
-    } else {
-        activeImg = 0;
-
-    }
 
 
-    /* for (let keyPicture of images) {
- 
-         cardPicture.innerHTML +=
-             '<img src="./' + `${keyPicture.image}` + '">';
-     }*/
+let activeImgCounter = 0;
 
+// ciclo per creare il 'DIV' in base alla lunghezza dell'array
 
+images.forEach((keyImages, index) => {
+
+    const cardImgElem = document.createElement('div');
+
+    // add la classe card-img
+
+    cardImgElem.classList.add('card-img');
+
+// se index dell'array e il contatore activeImgConter sono uguali add 'active' alla classe
+
+    if (index == activeImgCounter) cardImgElem.classList.add('active');
+
+    //scrivo nel html tutto l'arrey di oggetti ad ogni ciclo
+
+    cardImgElem.innerHTML = `
+    <img src="./${keyImages.image} ">
+    <div class="card-title-description m-2">
+    <h5 class="card-title text-end" id="title"> ${keyImages.title}</h5>
+    <p class="card-text text-end" id="description"> ${keyImages.text}</p>
+    </div>
+    `;
+
+    console.log(cardImgElem);
+
+//creo un nodo con la classe 'active'
+
+    keyImages.nodo = cardImgElem;
+    cardIdElem.append(cardImgElem);
+   
 });
+
+
+buttonNext.addEventListener('click', next)
+
+function next() {
+
+//creo una varibile e gli passo l'arrey con il nodo con la classe 'active'
+
+    const oldCardActive = images[activeImgCounter].nodo;
+
+    //rimuovo la classe 'active'
+
+    oldCardActive.classList.remove('active');
+
+//incremento il contatore
+
+    activeImgCounter++;
+    
+//se il contatore è minore della lunghezza dell'arrey add 'active' alla classe successiva
+//altrimenti quando il contatore è maggiore o uguale alla lunghezza dell'arrey, il contatore ritorna a 0
+
+    if (activeImgCounter >= images.length) activeImgCounter = 0;
+
+    const newCardActive = images[activeImgCounter].nodo;
+    newCardActive.classList.add('active');
+}
+
+next();
+
+buttonBack.addEventListener("click", back)
+
+function back() {
+
+//creo una varibile e gli passo l'arrey con il nodo con la classe 'active'
+
+    const oldCardActive = images[activeImgCounter].nodo;
+
+    //rimuovo la classe 'active'
+
+    oldCardActive.classList.remove('active');
+
+//decremento il contatore
+
+    activeImgCounter--;
+
+//se il contatore è minore di 0 la lunghezza dell'arrey va all'ultimo oggetto dell'arrey
+//altrimenti add 'active' alla classe precedente
+
+    if (activeImgCounter < 0) activeImgCounter = images.length - 1;
+
+        const newCardActive = images[activeImgCounter].nodo;
+    newCardActive.classList.add('active');
+
+};
+
+back();
